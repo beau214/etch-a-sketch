1c0d7f72e14c04a988626dd89f13ab0551fde6a2
Beau Williams <beaumatthew214@gmail.com>
2022-07-28 21:41:18 -0500

    Bare min working, will come back after learning grid

--- etch.js
+++ etch.js
@@ -1,15 +1,9 @@
-// for (i = 0; i < 16; i++) {
-//     for(j = 0; j < 16; j++){
-//         const board = document.querySelector('.board');
-//         const row = document.createElement('div');
-
-//         row.classList.add('row');
-//         board.appendChild(row);
-//     }
-// }
-
-function createGrid(n) {
-    
+let n = prompt('Enter a grid size: \nWill be grid x grid');
+if (n < 2 || n > 50){
+    console.log('grid needs to be at least 2 but 50 or less');
+    n = prompt('Enter a valid number:');
+}
+function createGrid(n) { 
     for(i = 0; i < n; i++){
         const board = document.querySelector('.board');
         const row = document.createElement('div');
@@ -17,24 +11,29 @@ function createGrid(n) {
         board.appendChild(row);
         for(j = 0; j < n; j++){
             const cell = document.createElement('div');
+            
             cell.classList.add('cell');
             row.appendChild(cell);
         }
     }
+    
 } 
-
-function setGrid(){
-    let size = document.querySelector('.grid').value;
-    createGrid(size);
-}
 
 createGrid(16);
 
 function clearGrid() {
-    const cell = document.querySelectorAll('.cell')
-    cell.forEach(cell => {
-        cell.setAttribute('style', 'background-color: white;');
+    const row = document.querySelectorAll('.row')
+    row.forEach(row => {
+        row.remove();
     });
+    createGrid(n);
+
+    const cell = document.querySelectorAll('.cell');
+    cell.forEach(cell => {
+    cell.addEventListener('mouseover', function() {
+        cell.setAttribute('style', 'background-color: black;');
+    });
+});
 }
 
 //Event Listeners
@@ -50,3 +49,24 @@ const reset = document.querySelector('.reset');
 reset.addEventListener('click', function() {
     clearGrid();
 });
+
+const newGrid = document.querySelector('.size');
+newGrid.addEventListener('click', function() {
+    // clearGrid();
+    n = prompt('Choose a new size');
+    if (n < 2 || n > 50){
+        console.log('grid needs to be at least 2 but 50 or less');
+        n = prompt('Enter a valid number:');
+    }
+    clearGrid();
+})
+
+// function newSize(size){
+//     clearGrid();
+    // const cell = document.querySelectorAll('.cell');
+    // cell.forEach(cell => {
+    // cell.addEventListener('mouseover', function() {
+    //     cell.setAttribute('style', 'background-color: black;');
+    // });
+    // })
+// }
